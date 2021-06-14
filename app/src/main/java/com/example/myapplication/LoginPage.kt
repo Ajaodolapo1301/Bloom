d6c7c77115da62c49bbb75dc3f51dcaa6638add2
Ajao Ataul-mujeeb <ajao@glade.ng>
2021-06-14 21:12:34 +0100

    first commit

--- app/src/main/java/com/example/myapplication/LoginPage.kt
+++ app/src/main/java/com/example/myapplication/LoginPage.kt
@@ -4,7 +4,7 @@ import android.graphics.Color
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
